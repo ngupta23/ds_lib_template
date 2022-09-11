e653effa85abae3d930b020271bf7c928e8de7f8
Nikhil Gupta <mywork.ng@gmail.com>
2022-09-11 12:27:29 -0500

    cleaned up example

--- analysis.py
+++ analysis.py
@@ -2,13 +2,17 @@ import logging
 
 import pandas as pd
 
-from ds_lib_template.outlier.deviation import (MADOutlierDetection,
-                                               StdDevOutlierDetection)
+from ds_lib_template.outlier.deviation import (
+    MADOutlierDetection,
+    StdDevOutlierDetection,
+)
 
+# Get data from your main data source (mocking it here)
 data = pd.Series(
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 1000]
 )
 
+# Get your logger from your environment (mocking it here)
 logger = logging.getLogger("my_logger")
 # formatter = logging.Formatter(
 #     "%(asctime)s | %(levelname)s | %(message)s", "%m-%d-%Y %H:%M:%S"
@@ -19,14 +23,10 @@ logger = logging.getLogger("my_logger")
 # logger.warning("WARN")
 # logger.error("ERROR")
 
+# Run your main code
 std_od = StdDevOutlierDetection(data=data, logger=logger)
 std_od.detect_outliers().correct_outliers()
 corrected = std_od.get_corrected_data()
-print(f"Corrected data (STD DEV): {corrected}")
+logger.info(f"Corrected data (STD DEV): {corrected}")
 
-mad_od = MADOutlierDetection(data=data, logger=logger)
-mad_od.detect_outliers().correct_outliers()
-corrected = mad_od.get_corrected_data()
-print(f"Corrected data (MAD): {corrected}")
-
-print("DONE")
+logger.info("DONE")
